66fbef090c63875ce7f2f63052025ebe149f6ff9
Fiction <goosefromhellyes@gmail.com>
2026-01-19 07:32:30 +0100

    init commit

---
+++ src/test/java/org/govnorganization/bookmanager/bookmweb/BookmWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.govnorganization.bookmanager.bookmweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
