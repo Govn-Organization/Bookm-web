1c1aac79540ea1a19124a6fb8d1a3a3239ddde92
FoxLys <FoxLys.com@gmail.com>
2026-01-21 05:27:33 +0100

    move frontend files to Bookm-web

---
+++ src/main/java/org/govnorganization/bookmanager/bookmweb/BookmWebApplication.java
@@ -0,0 +1,13 @@
+package org.govnorganization.bookmanager.bookmweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmWebApplication.class, args);
+    }
+
+}
