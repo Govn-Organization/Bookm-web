606b71cec2a19c2eb95dc2e06b2fa6143c91b382
Fiction <goosefromhellyes@gmail.com>
2026-01-30 16:28:37 +0100

    refac

--- src/main/java/org/govnorganization/bookmanager/bookmweb/config/OpenApiConfig.java
+++ src/main/java/org/govnorganization/bookmanager/bookmweb/config/OpenApiConfig.java
@@ -13,23 +13,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-    @Value("${app.base-url:http://localhost:8080}")
-    private String baseUrl;
+  @Value("${app.base-url:http://localhost:8080}")
+  private String baseUrl;
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-            .info(new Info()
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(
+            new Info()
                 .title("Book Manager API")
                 .version("1.0")
-                .description("API для управления книгами"))
-            .servers(List.of(
-                new Server().url(baseUrl).description("Current server")
-            ))
-            .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
-            .components(new io.swagger.v3.oas.models.Components()
-                .addSecuritySchemes("basicAuth", new SecurityScheme()
-                    .type(SecurityScheme.Type.HTTP)
-                    .scheme("basic")));
-    }
+                .description("API " + "для управления книгами"))
+        .servers(List.of(new Server().url(baseUrl).description("Current server")))
+        .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+        .components(
+            new io.swagger.v3.oas.models.Components()
+                .addSecuritySchemes(
+                    "basicAuth",
+                    new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")));
+  }
 }
